31125685e46ccad68295040b02d11356dda6ee91
Yamini Kumawat <yaminikumawat94@gmail.com>
2017-08-30 11:19:18 -0400

    some more to go

--- src/index.js
+++ src/index.js
@@ -54,3 +54,77 @@ function reduce(list, callback, initialValue) {
   }
   return acumulator
 }
+
+function find(list, callback){
+  if (Array.isArray(list)) {
+    for(let i = 0; i < list.length; i++){
+      if (callback(list[i])) {
+        return list[i]
+      }
+    }
+  } else if (typeof list === "object") {
+    for(let key in list) {
+      if (callback(list[key])){
+        return list[key]
+      }
+    }
+  }
+}
+
+function filter (list, callback) {
+  let newList
+  if (Array.isArray(list)) {
+    newList = []
+    for(let i = 0, n = list.length; i < n; i++){
+      if (callback(list[i])) {
+        newList.push(list[i])
+      }
+    }
+  } else if (typeof list === "object") {
+    newList = {}
+    for(let key in list) {
+      if (callback(list[key])){
+        let value = list[key]
+        newList = Object.assign(newList, {[key]: value})
+      }
+    }
+  }
+  return newList
+}
+
+// function sortBy(list, callback) {
+//   if (Array.isArray(list)) {
+//     for(let i = 0, n = list.length; i < n; i++){
+//       callback(list[i])
+//     }
+//   } else if (typeof list === "object"){
+//
+//   }
+// }
+
+
+function size(list) {
+  if (Array.isArray(list)) {
+    return list.length
+  } else if (typeof list === "object"){
+    return Object.keys(list).length
+  }
+}
+
+function first(array, n = 1) {
+  return array.slice(0, n)
+}
+
+function last(array, n = 1) {
+  return array.slice(-n)
+}
+
+function compact(array) {
+  let newArray = []
+  let falsy = [false, null, 0, "", undefined, NaN]
+  filter(array, element => {
+    if (!falsy.includes(element))
+    {newArray.push(element)}
+  })
+    return newArray
+}
